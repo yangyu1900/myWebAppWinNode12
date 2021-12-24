3147f1db2d0fae80378766b75a17fe49aa0e3ee7
yangyu1900 <40253530+yangyu1900@users.noreply.github.com>
2021-12-24 14:23:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ var app = express()
 app.get('/', (req, res) => res.json(process.env))
 
 app.get('/throwError', (req, res) => {
-    fs.openSync(process.env.HOME + '/non-exist-file.txt');
+    setTimeout(() => fs.openSync(process.env.HOME + '/non-exist-file.txt'), 3000);
     res.send('OK');
 })
 
-app.listen(port);
+app.listen(port, () => {
+    var moduleLoadedLogFilePath = process.env.HOME + '/LogFiles/ModuleLoaded.log';
+    if (fs.existsSync(moduleLoadedLogFilePath)) {
+        fs.unlinkSync(moduleLoadedLogFilePath);
+    }
+    for (let m of Object.keys(require('module')._cache))
+    {
+        fs.appendFileSync(moduleLoadedLogFilePath, m + '\n');
+    }
+});
